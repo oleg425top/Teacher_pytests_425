4a6210d9e7cee2be3a73ae5642120f45e1668ef4
oleg425top <oleg59152@gmail.com>
2024-12-18 14:04:42 +0500

    update Fixture, add file test_class_teacher_pytest.py

--- teacher_pytests/test_teacher_pytests/conftest.py
+++ teacher_pytests/test_teacher_pytests/conftest.py
@@ -7,3 +7,9 @@ def teacher():
     Teacher.teacher_dict.clear()
     teacher = Teacher('test_name', 'test_education', 99)
     return teacher
+
+@pytest.fixture()
+def discipline_teacher():
+    DisciplineTeacher.discipline_teacher_dict.clear()
+    discipline_teacher = DisciplineTeacher('new_name','new_education', 66, 'test_discipline', 'test_job_title')
+
